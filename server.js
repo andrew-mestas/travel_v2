58a5a1b00d5c4e4bd053406cbba926766f59236c
Phil Nguyen <philnguyen@Phils-MacBook-Pro.local>
2016-06-26 22:42:15 -0700

    Added server.js and a few new script files

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
+if (!process.env.APP_SECRET) throw new Error('you need an APP_SECRET env variable');
+
 const express = require('express');
 const mongoose = require('mongoose');
 
 const app = express();
 const PORT = process.env.PORT || 5555;
 
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/utravel_db');
+
 app.get('/', (req, res) => {
   res.send({MSG: 'API'});
 });
+
+app.use(express.static(__dirname + '/build'));
 
 module.exports = exports = app.listen(PORT, () => console.log('server up on port: ' + PORT));
